98c95df450ce99e3a922bef40e7144241b4e7265
Lam Chan <lam.chan@avant.com>
2022-09-05 20:55:00 -0700

    adding LLB stuff

---
+++ building-LLB-with-buildkit/builder.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"github.com/moby/buildkit/client/llb"
+	"os"
+)
+
+func main() {
+
+	dt, err := createLLBState().Marshal(context.TODO(), llb.LinuxAmd64)
+	if err != nil {
+		panic(err)
+	}
+	llb.WriteTo(dt, os.Stdout)
+}
+
+func createLLBState() llb.State {
+	return llb.Image("docker.io/library/alpine").
+		File(llb.Copy(llb.Local("context"), "README.md", "README.md")).
+		Run(llb.Args([]string{"/bin/sh", "-c", "echo \"programmatically built\" > /built.txt"})).
+		Root()
+}
